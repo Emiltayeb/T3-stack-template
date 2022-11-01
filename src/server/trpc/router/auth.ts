b85ef39c8e0e4f6ea98531fdbd4ff44e66d54c46
Emil Tayeb <emiltayeb912@gmail.com>
2022-11-01 17:31:49 +0200

    init

--- src/server/trpc/router/auth.ts
+++ src/server/trpc/router/auth.ts
@@ -1,9 +1,9 @@
 import { router, publicProcedure, protectedProcedure } from "../trpc";
 
 export const authRouter = router({
-  getSession: publicProcedure.query(({ ctx }) => {
-    return ctx.session;
-  }),
+  // getSession: publicProcedure.query(({ ctx }) => {
+  //   return ctx.session;
+  // }),
   getSecretMessage: protectedProcedure.query(() => {
     return "You are logged in and can see this secret message!";
   }),
