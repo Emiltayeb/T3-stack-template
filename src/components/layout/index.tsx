f6bf5e836da27352274305d885eb8bccb3419dd7
Emil Tayeb <emiltayeb912@gmail.com>
2022-11-05 18:29:43 +0200

    refactor

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
 
 type LayoutProps = {
-  children:React.ReactNode
-}
-const Layout  = function (props:LayoutProps) {
-  return (
-    <div className="container  mx-auto min-h-screen  p-4">
-      {props.children}
-    </div>
-  );
+  children: React.ReactNode;
+};
+const Layout = function (props: LayoutProps) {
+  return <>{props.children}</>;
 };
 
 export default Layout;
